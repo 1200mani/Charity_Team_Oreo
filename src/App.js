ddda5e632aeeec759f374aba298bf0af0715f042
nabiya <sheikh.nabiyaa@gmail.com>
2025-01-25 15:00:04 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Contact from './ContactPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<h1>Hello! Welcome to Charity Websitegit</h1>} />
+        <Route path="/contact" element={<Contact />} /> {/* Add the Contact route */}
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
+
